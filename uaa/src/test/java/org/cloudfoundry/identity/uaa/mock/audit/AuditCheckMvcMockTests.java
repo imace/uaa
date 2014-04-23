80644eabd1e2b7a23775c5dae9640c5f16501dc5
Filip Hanik <fhanik@gopivotal.com>
2014-04-23 11:47:13 -0600

    WIP

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/audit/AuditCheckMvcMockTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/audit/AuditCheckMvcMockTests.java
@@ -1,30 +1,30 @@
 package org.cloudfoundry.identity.uaa.mock.audit;
 
-import org.cloudfoundry.identity.uaa.audit.AuditEvent;
 import org.cloudfoundry.identity.uaa.audit.LoggingAuditService;
 import org.cloudfoundry.identity.uaa.audit.event.AbstractUaaEvent;
 import org.cloudfoundry.identity.uaa.authentication.event.BadCredentialsListener;
+import org.cloudfoundry.identity.uaa.authentication.event.UserAuthenticationSuccessEvent;
 import org.cloudfoundry.identity.uaa.config.YamlServletProfileInitializer;
-import org.cloudfoundry.identity.uaa.oauth.event.ClientAdminEventPublisher;
 import org.cloudfoundry.identity.uaa.test.TestClient;
 import org.cloudfoundry.identity.uaa.test.UaaTestAccounts;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationListener;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,15 +35,19 @@ public class AuditCheckMvcMockTests {
     private TestClient testClient = null;
     private UaaTestAccounts testAccounts = null;
     private ApplicationEventPublisher applicationEventPublisher = null;
-    private ArgumentCaptor<AuditEvent> captor = null;
+    private ArgumentCaptor captor = null;
     private BadCredentialsListener badCredentialsListener;
     private LoggingAuditService loggingAuditService;
+    private ApplicationListener<?> listener = null;
 
     @Before
     public void setUp() throws Exception {
+        listener = mock(ApplicationListener.class);
+
         webApplicationContext = new XmlWebApplicationContext();
         webApplicationContext.setServletContext(new MockServletContext());
         webApplicationContext.setConfigLocation("file:./src/main/webapp/WEB-INF/spring-servlet.xml");
+        webApplicationContext.addApplicationListener(listener);
         new YamlServletProfileInitializer().initialize(webApplicationContext);
         webApplicationContext.refresh();
         FilterChainProxy springSecurityFilterChain = webApplicationContext.getBean(FilterChainProxy.class);
@@ -54,12 +58,13 @@ public class AuditCheckMvcMockTests {
         testAccounts = UaaTestAccounts.standard(null);
 
         loggingAuditService = webApplicationContext.getBean(LoggingAuditService.class);
-        captor = ArgumentCaptor.forClass(AuditEvent.class);
+
 
     }
 
     @Test
     public void userLoginTest() throws Exception {
+        captor = ArgumentCaptor.forClass(UserAuthenticationSuccessEvent.class);
         MockHttpServletRequestBuilder loginPost = post("/login.do")
             .accept(MediaType.TEXT_HTML_VALUE)
             .param("username", testAccounts.getUserName())
@@ -69,6 +74,7 @@ public class AuditCheckMvcMockTests {
         result.andExpect(status().is3xxRedirection())
             .andExpect(header().string("Location","/"));
 
+        verify(listener, times(1));
 
     }
 }
